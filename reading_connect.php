5203abda8cea61b56d7052e36ec810a9169ea211
110113057 <h85201021@gmail.com>
2014-01-03 13:03:54 +0800

    Update reading_connect.php

--- reading_connect.php
+++ reading_connect.php
@@ -2,77 +2,69 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <?php
-include("mysql_connect.php"); 
-$id = $_POST['id']; 
+include("mysql_connect.php");
+$id = $_POST['id'];
 $pw = $_POST['pw'];
 
 $sql = "SELECT * FROM member where account='$id' and password='$pw'";
 $row = mysql_fetch_array(mysql_query($sql));
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_學習網</title>
+<title>第六組的教學學習網</title>
 </head>
 
 <body>
 <div id="HEADER">
-	<h2>電腦科學學習網</h2>
+	<h2>數學遊戲學習網</h2>
 </div>
 <div id="MAIN_NAV">
 	<ul>
-		<li><b>開始閱讀</b></li>
+		<li><a href="reading.php">開始閱讀</a></li>
 		<li><a href="upload.php">檔案上傳</a></li>
 		<li><a href="message.php">留言板</a></li>
-		<li><a href="record.php">歷史紀錄</a></li>
+		<li><b>歷史紀錄</b></li>
 		<li><a href="login.php">會員資料修改</a></li>
 		<li><a href="group.php">管理團隊</a></li>
 		<li><a href="manager_login.php">管理者專區</a></li>
 		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li>
 	</ul>
 </div>
-<div id="CONTENT"> 
+<div id="CONTENT">
 	<p>
 	
 		<center>
-		<h2><?php echo $id; ?>，請選擇閱讀材料<br/></h2>
+		<h2>閱讀紀錄查詢<br/></h2>
 		<br/>
 		<br/>
 		
 		<?php
 			if (!$row) { 
 				echo "<h3>帳號或密碼錯誤，請重新輸入</h3>";
-        		echo '<meta http-equiv=REFRESH CONTENT=2;url=reading.php>';
+        		echo '<meta http-equiv=REFRESH CONTENT=2;url=record.php>';
 			}
 			else {
-				$sql = "SELECT * FROM reading";
+				$sql = "SELECT record.account, reading.name, record.time, record.comments FROM record JOIN reading ON reading.serial=record.serial WHERE record.account='$id'";
 				$result = mysql_query($sql);
 											
 				if (!$result) { 
 					die('Invalid query: ' . mysql_error());
 				}
 				
-				echo "<form name='form' method='post' action='reading_start.php'>"; 
-				echo "<input type='hidden' name='id' value=".$id." />";
-				echo "<input type='hidden' name='pw' value=".$pw." />";
-								
-				echo "<table width=800 border=1>";            
-				echo "<tr align=center><td>選取</td><td>編號</td><td>類型</td><td>資料名</td></tr>";		
+				echo "<table width=800 border=1>";
+				echo "<tr align=center><td>姓名</td><td>資料名</td><td>時間</td><td>評論</td></tr>";		
 					
-				while($row = mysql_fetch_array($result)){     
-					
-					echo "<tr align=center><td><input name='select' type='radio' value=".$row['serial']."+".$row['type']."></td>"; //radio屬性，value輸出的內容				
-					echo "<td>".$row['serial']."</td>";
-					echo "<td>".$row['type']."</td>";
+				while($row = mysql_fetch_array($result)){ 
+					echo "<tr align=center><td>".$row['account']."</td>";
 					echo "<td>".$row['name']."</td>";
+					echo "<td>".$row['time']."</td>";
+					echo "<td>".$row['comments']."</td>";
 					echo "</tr>";
 				}
 				echo "</table>";
-				echo "<input type='submit' name='button' value='開始閱讀' />";	
-				echo "</form>";
 			}
 		?>
-		<br/><br/><br/><br/><br/><br/><br/><br/><br/>
-		<form name="form" method="post" action=" reading.php">
-			<input type="submit" name="button" value="回上一頁" />	 
+		<form name="form" method="post" action=" record.php">
+			<input type="submit" name="button" value="回上一頁" />	
 		</form>
 		
 		</center>
@@ -83,7 +75,9 @@ $row = mysql_fetch_array(mysql_query($sql));
 <div id="FOOTER">	
 	<p>
 		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Gung-Si Chen</i> </center></h2>
+		<h2><center><br/>
+	      Author by吳映萱&amp;劉柱恩&amp;戴君翰&amp;林利群&amp;李姿盈&amp;曾苡筑<i></i>
+		</center></h2>
 	</p>
 </div>
 </body>
