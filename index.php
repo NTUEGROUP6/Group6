76ca65416298ddb7ad746c783f0f7b2dab798db3
110113057 <h85201021@gmail.com>
2014-01-03 12:38:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_學習網</title>
+<title>第六組的教學學習網</title>
 </head>
 
 <body>
 
 
-	<h2>電腦科學學習網</h2>
+	<h2>數學遊戲學習網</h2>
 
 <div id="MAIN_NAV" >
 
@@ -25,20 +25,22 @@
 <div id="CONTENT" >   
 	<p>
 		<center>
-		<h2>最新公告<br/></h2>
+		<h2>歡迎來到數學學習網<br/></h2>
 		<table>
 		<tr>
-			<td> <h4>請點選上方連結以使用相關功能<br/></td>
+			<td width="339" height="33">2014.1.3 編輯網站內容<br/></td>
 		</tr>
 		</table>
-		<br/><br/><br/><br/><br/><br/></h4>
+		<br/><br/><br/><br/><br/><br/>
 		</center>
 	</p>
 </div>
 <div id="FOOTER">	
 	<p>
 		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Gung-Si Chen</i> </center></h2>
+		<h2><center><br/>
+	      Author by 吳映萱&amp;劉柱恩&amp;戴君翰&amp;林利群&amp;李姿盈&amp;曾苡筑<i></i>
+		</center></h2>
 	</p>
 </div>
 </body>
